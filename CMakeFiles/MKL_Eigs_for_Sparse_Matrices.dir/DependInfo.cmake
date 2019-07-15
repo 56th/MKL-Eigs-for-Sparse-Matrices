be388e296c9ce4aa0ee80ffbf13a7f9c19210ac6
Alexander Zhiliakov <alex@math.uh.edu>
2019-07-15 02:11:35 -0500

    Optimize eig value comp

--- CMakeFiles/MKL_Eigs_for_Sparse_Matrices.dir/DependInfo.cmake
+++ CMakeFiles/MKL_Eigs_for_Sparse_Matrices.dir/DependInfo.cmake
@@ -11,7 +11,6 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/home/alexander/intel/compilers_and_libraries_2019.1.144/linux/mkl/include"
   "../Singleton-Logger/sln/Sources"
   )
 
